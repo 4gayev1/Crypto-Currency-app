e32215a85d5334bf6b098817cb3bc4f6c588163e
Vahid <Vahid.Agayev@kapitalbank.az>
2024-07-15 13:39:59 +0400

    refactoring

--- src/Coin.js
+++ src/Coin.js
@@ -1,37 +1,24 @@
 import React from "react";
 import "./Coin.css";
 
-const Coin = ({
-  name,
-  price,
-  symbol,
-  image,
-  priceChange
-}) => {
+const Coin = ({ name, price, symbol, image, priceChange }) => {
   return (
-    <div className='coin-container'>
+    <div className="coin-container">
+      <div className="coin">
+        <img src={image} alt="crypto" />
+        <h1>{name}</h1>
+        <p className="coin-symbol">{symbol}</p>
+      </div>
 
+      <div className="coin-data">
+        <p className="coin-price">${price}</p>
 
-        <div className='coin'>
-          <img src={image} alt='crypto' />
-          <h1>{name}</h1>
-          <p className='coin-symbol'>{symbol}</p>
-        </div>
-
-        <div className='coin-data'>
-
-          <p className='coin-price'>${price}</p>
-
-          {priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
-          ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
-          )}
-
-          
-        </div>
-
-
+        {priceChange < 0 ? (
+          <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+        ) : (
+          <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+        )}
+      </div>
     </div>
   );
 };
